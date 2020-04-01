6ae70df7b86d886b32259cc4cf808de359eff159
Kenneth Spatta <kenyounot@gmail.com>
2020-04-01 01:42:27 -0400

    Add all placeholder routes for logs

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,6 +1,49 @@
+// @desc Get all logs
+// @route GET /api/v1/logs
+// @auth Private
 exports.getLogs = (req, res) => {
 	res.status(200).json({
 		success: true,
 		msg: 'Get all logs'
+	});
+};
+
+// @desc Create a log
+// @route POST /api/v1/logs
+// @auth Private
+exports.createLog = (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: 'Create a log'
+	});
+};
+
+// @desc Get a single log
+// @route GET /api/v1/logs/:id
+// @auth Private
+exports.getLog = (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Get log with id of ${req.params.id}`
+	});
+};
+
+// @desc Update a single log
+// @route PUT /api/v1/logs/:id
+// @auth Private
+exports.updateLog = (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Update log with id of ${req.params.id}`
+	});
+};
+
+// @desc Delete a single log
+// @route DELETE /api/v1/logs/:id
+// @auth Private
+exports.deleteLog = (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Delete log with id of ${req.params.id}`
 	});
 };
--- routes/logs.js
+++ routes/logs.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getLogs } = require('../controllers/logs');
+const {
+	getLogs,
+	getLog,
+	createLog,
+	updateLog,
+	deleteLog
+} = require('../controllers/logs');
 
-router.route('/').get(getLogs);
+router
+	.route('/')
+	.get(getLogs)
+	.post(createLog);
+
+router
+	.route('/:id')
+	.get(getLog)
+	.put(updateLog)
+	.delete(deleteLog);
 
 module.exports = router;
