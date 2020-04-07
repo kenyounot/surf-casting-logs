35d39e545231ebf68c6ffcd52bc18c13a3c514f4
Kenneth Spatta <kenyounot@gmail.com>
2020-04-07 01:02:49 -0400

    Add rest of log model attributes and validations, and exported the model.

--- config/models/Log.js
+++ config/models/Log.js
@@ -58,19 +58,56 @@ const logSchema = new mongoose.Schema({
 	},
 	airTemperature: {
 		type: Number,
-		maxlength: [3, 'Temperature cant be more than 3 digits.'],
+		min: [0, 'Minimum air temperature is 0.'],
+		max: [120, 'Maximum air temperature is 120'],
 		required: [true, 'Air temperature is required.'],
 	},
 	barometer: {
 		type: String,
 		maxlength: [
 			20,
-			'Max length for baromete cant be more than 20 characters',
+			'Max length for barometer cant be more than 20 characters',
 		],
-		required: [true, 'Baromete is required.'],
+		required: [true, 'Barometer is required.'],
 	},
 	swellSize: {
 		type: Number,
-		maxlength: [2, 'Max swell size length is 2 digits.'],
+		min: [0, 'Minimum swell size is 0.'],
+		max: [15, 'Maximum swell size is 15'],
+		required: [true, 'Swell size is required.'],
+	},
+	tide: {
+		type: String,
+		maxLength: [
+			30,
+			'Max tide length cant be more than 30 characters.',
+		],
+		required: [true, 'Tide is required.'],
+	},
+	waterClarity: {
+		type: String,
+		maxLength: [
+			30,
+			'Max water clarity length cant be more than 30 characters.',
+		],
+		required: [true, 'Water clarity is required.'],
+	},
+	waterTemperature: {
+		type: Number,
+		min: [28, 'Min water temperature is 28.'],
+		max: [90, 'Max water temperature is 90.'],
+		required: [true, 'Water temperature is required.'],
+	},
+	tripRating: {
+		type: Number,
+		min: [1, 'Minimum rating is 1.'],
+		max: [5, 'Maximum rating is 5.'],
+		required: true,
+	},
+	comments: {
+		type: String,
+		maxLength: [1000, 'Max comment length is 1000 characters.'],
 	},
 });
+
+module.exports = mongoose.model('Log', logSchema);
